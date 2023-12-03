c4a4d394fd11e9e8d12436a0153f2934f4e62a4b
coolqwebs <asankadyrovnk@gmail.com>
2023-12-03 19:25:15 +0600

    favicon & copyPlugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,9 +2,11 @@ import HtmlWebpackPlugin from "html-webpack-plugin";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
 import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin";
+import CopyPlugin from "copy-webpack-plugin";
 import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
 import { Configuration, DefinePlugin, ProgressPlugin } from "webpack";
 import { BuildOptions } from "./types/types";
+import path from "path";
 
 export function buildPlugins({
   mode,
@@ -18,17 +20,20 @@ export function buildPlugins({
   const plugins: Configuration["plugins"] = [
     new HtmlWebpackPlugin({
       template: paths.html,
+      favicon: path.resolve(paths.public, "favicon.ico"),
     }),
     new DefinePlugin({
       PLATFORM: JSON.stringify(platform),
       MODE: JSON.stringify(mode),
     }),
   ];
+
   if (isDev) {
     plugins.push(new ProgressPlugin());
     plugins.push(new ForkTsCheckerWebpackPlugin());
     plugins.push(new ReactRefreshWebpackPlugin());
   }
+
   if (isProd) {
     plugins.push(
       new MiniCssExtractPlugin({
@@ -36,9 +41,21 @@ export function buildPlugins({
         chunkFilename: "css/[name].[contenthash:8].css",
       })
     );
+    plugins.push(
+      new CopyPlugin({
+        patterns: [
+          {
+            from: path.resolve(paths.public, "locales"),
+            to: path.resolve(paths.output, "locales"),
+          },
+        ],
+      })
+    );
   }
+
   if (analyzer) {
     plugins.push(new BundleAnalyzerPlugin());
   }
+
   return plugins;
 }
--- webpack.config.ts
+++ webpack.config.ts
@@ -18,6 +18,7 @@ export default (env: EnvVariables) => {
       html: path.resolve(__dirname, "public", "index.html"),
       output: path.resolve(__dirname, "build"),
       src: path.resolve(__dirname, "src"),
+      public: path.resolve(__dirname, "public"),
     },
     analyzer: env.analyzer,
     platform: env.platform ?? "desktop",
